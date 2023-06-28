dbbd39ca9ae8b67d2d4d15156140d666cb66a88f
arvind_tomar1999 <arvind.tomar@innovasolutions.com>
2023-06-28 18:02:46 +0530

    Created the todo form with 2 fields with css using mat-ui

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,70 @@
 import React from 'react'
+import { useState } from 'react'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Button, TextField } from '@mui/material';
+
 
 export default function TodoForm() {
+  const [items, setItems] = useState([{ item: '', text: '' }]);
+  const handleChange = (index, event) => {
+    const { name, value } = event.target;
+    const newItems = [...items];
+    newItems[index] = { ...newItems[index], [name]: value };
+    setItems(newItems);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleAddItem = () => {
+    setItems([...items, { item: '', text: '' }]);
+  };
+
   return (
-    <div>TodoForm</div>
+    <>
+      <form onSubmit={handleSubmit}>
+        {items.map((item, index) => (
+          <div key={index}>
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel id={`item-label-${index}`}>Todo</InputLabel>
+              <Select
+                labelId={`item-label-${index}`}
+                id={`item-select-${index}`}
+                name="item"
+                value={item.item}
+                label="item"
+                onChange={(event) => handleChange(index, event)}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="Action Items">Action Items</MenuItem>
+                <MenuItem value="Project Scope">Project Scope</MenuItem>
+                <MenuItem value="Value Adds">Value Adds</MenuItem>
+              </Select>
+
+              <TextField
+                id={`text-field-${index}`}
+                name="text"
+                value={item.text}
+                label="Description"
+                variant="outlined"
+                onChange={(event) => handleChange(index, event)}
+                sx={{ my: 2 }}
+              />
+            </FormControl>
+
+          </div>
+        ))}
+
+        <Button variant="contained" type="submit" style={{ margin: "10px" }}>Submit</Button>
+        <Button variant="contained" type="button" color="success" onClick={handleAddItem}>Add</Button>
+
+      </form>
+    </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import TodoForm from '../components/TodoForm'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <TodoForm />
+    </div>
   )
 }
