6e7b3805aac3a9ef329a0e220634e767e05cee7f
arvind_tomar1999 <arvind.tomar@innovasolutions.com>
2023-06-29 17:21:03 +0530

    integrate the api's to get and delete tasks

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -5,88 +5,127 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { Button, TextField } from "@mui/material";
-import ClearIcon from "@mui/icons-material/Clear";
-import { left, right } from "@popperjs/core";
+import { useEffect } from "react";
+import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
+import EditSharpIcon from '@mui/icons-material/EditSharp';
 
 export default function TodoForm() {
-  const [items, setItems] = useState([{ item: "", text: "" }]);
-  const handleChange = (index, event) => {
-    const { name, value } = event.target;
-    const newItems = [...items];
-    newItems[index] = { ...newItems[index], [name]: value };
-    setItems(newItems);
+  const [task, setTask] = useState('');
+  const [description, setDescription] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const getTodo = async () => {
+    try {
+      const response = await fetch('http://localhost:3003/api/todo/getTodo');
+      const result = await response.json();
+      if (result.status) {
+        setTodos(result.data);
+      } else {
+        console.error('Fetch todos error:', result.message);
+      }
+    } catch (err) {
+      console.error('Fetch todos error:', err);
+    }
   };
 
-  const handleSubmit = (event) => {
+  const addTodo = async (event) => {
     event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3003/api/todo/addTodo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ task, description }),
+      });
+      if (!response.ok) {
+        throw new Error('Error adding todo');
+      }
+
+      const result = await response.json();
+      if (result.status) {
+        await getTodo()
+        setTask('');
+        setDescription('');
+      } else {
+        console.error('Add todo error:', result.message);
+      }
+    } catch (err) {
+      console.error('Add todo error:', err);
+    }
   };
 
-  const handleAddItem = () => {
-    setItems([...items, { item: "", text: "" }]);
+  const deletedTodo = () => {
+
+  }
+
+  const updateTodo = () => {
+
+  }
+
+  useEffect(() => {
+    getTodo();
+  }, []);
+
+  const handleTaskChange = (event) => {
+    setTask(event.target.value);
   };
 
-  const handleDelete = (index) => {
-    const newItems = [...items];
-    newItems.splice(index, 1);
-    setItems(newItems);
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
   };
+
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        {items.map((item, index) => (
-          <div key={index}>
-            {items.length === 1 ? (
-              ""
-            ) : (
-              <ClearIcon
-                onClick={() => handleDelete(index)}
-                sx={{ float: "right" }}
-              />
-            )}
-            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-              <InputLabel id={`item-label-${index}`}>Todo</InputLabel>
-              <Select
-                labelId={`item-label-${index}`}
-                id={`item-select-${index}`}
-                name="item"
-                value={item.item}
-                label="item"
-                onChange={(event) => handleChange(index, event)}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value="Action Items">Action Items</MenuItem>
-                <MenuItem value="Project Scope">Project Scope</MenuItem>
-                <MenuItem value="Value Adds">Value Adds</MenuItem>
-              </Select>
+      <form onSubmit={addTodo}>
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+          <InputLabel id="task-label">Todo</InputLabel>
+          <Select
+            labelId="task-label"
+            id="task-select"
+            value={task}
+            label="Todo"
+            onChange={handleTaskChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value="Action Items">Action Items</MenuItem>
+            <MenuItem value="Project Scope">Project Scope</MenuItem>
+            <MenuItem value="Value Adds">Value Adds</MenuItem>
+          </Select>
+          <TextField
+            id="description-field"
+            value={description}
+            label="Description"
+            variant="outlined"
+            sx={{ my: 2 }}
+            onChange={handleDescriptionChange}
+          />
+          <Button variant="contained" type="submit" style={{ margin: '10px' }}>
+            Add
+          </Button>
+        </FormControl>
 
-              <TextField
-                id={`text-field-${index}`}
-                name="text"
-                value={item.text}
-                label="Description"
-                variant="outlined"
-                onChange={(event) => handleChange(index, event)}
-                sx={{ my: 2 }}
-              />
-            </FormControl>
+
+      </form>
+
+      <div>
+        <h2>Todo List</h2>
+        {todos.map((todo, index) => (
+          <div key={index}>
+            <p>
+              <strong>Task:</strong> <span style={{ margin: "10px" }}> {todo.task} </span>
+              <strong>Description:</strong>  <span style={{ margin: "10px" }}>{todo.description}</span>
+              <strong>Actions:</strong>  <span style={{ margin: "10px" }}>
+                <DeleteSharpIcon onClick={() => deletedTodo(todo._id)} />
+                <EditSharpIcon onClick={updateTodo} />
+              </span>
+            </p>
           </div>
         ))}
-
-        <Button variant="contained" type="submit" style={{ margin: "10px" }}>
-          Submit
-        </Button>
-        <Button
-          variant="contained"
-          type="button"
-          color="success"
-          onClick={handleAddItem}
-        >
-          Add
-        </Button>
-      </form>
+      </div>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import TodoForm from '../components/TodoForm'
 export default function Home() {
   return (
     <div className='home'>
+      <h1>Todo</h1>
       <TodoForm />
     </div>
   )
