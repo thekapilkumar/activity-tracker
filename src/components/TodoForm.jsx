d18f09751e206ecfa8a3507078e4a1495f857780
Kapil Kumar <kapil.kumar@innovasolutions.com>
2023-06-29 17:49:56 +0530

    add delete functionality

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -6,62 +6,81 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { Button, TextField } from "@mui/material";
 import { useEffect } from "react";
-import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
-import EditSharpIcon from '@mui/icons-material/EditSharp';
+import DeleteSharpIcon from "@mui/icons-material/DeleteSharp";
+import EditSharpIcon from "@mui/icons-material/EditSharp";
 
 export default function TodoForm() {
-  const [task, setTask] = useState('');
-  const [description, setDescription] = useState('');
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
   const [todos, setTodos] = useState([]);
 
   const getTodo = async () => {
     try {
-      const response = await fetch('http://localhost:3003/api/todo/getTodo');
+      const response = await fetch("http://localhost:3003/api/todo/getTodo");
       const result = await response.json();
       if (result.status) {
         setTodos(result.data);
       } else {
-        console.error('Fetch todos error:', result.message);
+        console.error("Fetch todos error:", result.message);
       }
     } catch (err) {
-      console.error('Fetch todos error:', err);
+      console.error("Fetch todos error:", err);
     }
   };
 
   const addTodo = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:3003/api/todo/addTodo', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3003/api/todo/addTodo", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ task, description }),
       });
       if (!response.ok) {
-        throw new Error('Error adding todo');
+        throw new Error("Error adding todo");
       }
 
       const result = await response.json();
       if (result.status) {
-        await getTodo()
-        setTask('');
-        setDescription('');
+        await getTodo();
+        setTask("");
+        setDescription("");
       } else {
-        console.error('Add todo error:', result.message);
+        console.error("Add todo error:", result.message);
       }
     } catch (err) {
-      console.error('Add todo error:', err);
+      console.error("Add todo error:", err);
     }
   };
 
-  const deletedTodo = () => {
+  const deletedTodo = async (id) => {
+    const confirmDelete = window.confirm("Do you want to delete this item?");
+    if (confirmDelete) {
+      try {
+        const result = await fetch(
+          `http://localhost:3003/api/todo/deleteTodo/${id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const response = await result.json();
+        if (response.status) {
+          await getTodo();
+        } else {
+          console.error(response.message);
+        }
+      } catch (err) {
+        console.log("Error deleting todo...", err);
+      }
+    }
+  };
 
-  }
-
-  const updateTodo = () => {
-
-  }
+  const updateTodo = () => {};
 
   useEffect(() => {
     getTodo();
@@ -74,7 +93,6 @@ export default function TodoForm() {
   const handleDescriptionChange = (event) => {
     setDescription(event.target.value);
   };
-
 
   return (
     <>
@@ -103,12 +121,10 @@ export default function TodoForm() {
             sx={{ my: 2 }}
             onChange={handleDescriptionChange}
           />
-          <Button variant="contained" type="submit" style={{ margin: '10px' }}>
+          <Button variant="contained" type="submit" style={{ margin: "10px" }}>
             Add
           </Button>
         </FormControl>
-
-
       </form>
 
       <div>
@@ -116,9 +132,12 @@ export default function TodoForm() {
         {todos.map((todo, index) => (
           <div key={index}>
             <p>
-              <strong>Task:</strong> <span style={{ margin: "10px" }}> {todo.task} </span>
-              <strong>Description:</strong>  <span style={{ margin: "10px" }}>{todo.description}</span>
-              <strong>Actions:</strong>  <span style={{ margin: "10px" }}>
+              <strong>Task:</strong>{" "}
+              <span style={{ margin: "10px" }}> {todo.task} </span>
+              <strong>Description:</strong>{" "}
+              <span style={{ margin: "10px" }}>{todo.description}</span>
+              <strong>Actions:</strong>{" "}
+              <span style={{ margin: "10px" }}>
                 <DeleteSharpIcon onClick={() => deletedTodo(todo._id)} />
                 <EditSharpIcon onClick={updateTodo} />
               </span>
